6c55dd47f75a3b7f819e0743ea16b8c2904d558a
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-07-22 00:24:41 -0400

    added to the About me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -18,7 +18,7 @@ function About() {
       </div>
       <div className='left'>
         <p>
-         
+         Hi, My name is Michael Kuney and i am a very enthusiastic and eag individual willin got take on new challenges.  I have been a Culinary Chef for 24+ years and studied at C.I.A., The Culinary Institute of America. i am now persuing a career in full stack web development and will be graduatig from UNC Chapel Hill's FSF Bootcamp July of 2022.  Can't wait to get out there and see where this new career will take me!!
         </p>
       </div>
     </section>
